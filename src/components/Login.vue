ca2727230ed60f2c835e14fe4177275671d6eeb8
unknown <wuyong>
2019-12-01 21:42:53 +0800

    结构初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>login-component</h1>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+    h1{
+        font-size: 3em;
+        color: hotpink
+    }
+</style>
